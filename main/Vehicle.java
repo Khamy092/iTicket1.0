f772029bd7e22bd56429a4592d5b6c13e31ac012
Taqi Khaliqdad <khamy092@mymail.unisa.edu.au>
2024-06-26 15:21:38 +0930

    implement basic attributes and methods.

--- main/Vehicle.java
+++ main/Vehicle.java
@@ -1,5 +1,40 @@
 package main;
 
+/**
+ * Vehicle class - Represents a vehicle with a unique id and a plate number.
+ */
 public class Vehicle {
-    
+
+    private int id;
+    private String plateNumber;
+
+    // Constructor
+    public Vehicle(String plateNumber) {
+        this.id = Math.abs(plateNumber.hashCode());
+        this.plateNumber = plateNumber;
+    }
+
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "id=" + id +
+                ", plateNumber='" + plateNumber + '\'' +
+                '}';
+    }
+
 }
